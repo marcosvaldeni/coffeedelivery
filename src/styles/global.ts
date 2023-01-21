31e5d9b57586019372f81716a6872fc7f9bf9991
Marcos Valdeni Lucas <marcosvaldeni@gmail.com>
2023-01-21 19:20:26 +0000

    default layout

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,11 +18,11 @@ export const GlobalStyle = createGlobalStyle`
 		-webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea, button {
+  body, input, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 1rem;
     line-height: 130%;
-		color: ${props => props.theme.white};
+		color: ${props => props.theme['base-text']};
   }
 `;
