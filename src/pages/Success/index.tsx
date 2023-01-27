15b927b05955c975339dc38e9da303e7e0ab0b46
Marcos Valdeni Lucas <marcosvaldeni@gmail.com>
2023-01-27 19:17:43 +0000

    success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,55 @@
+import { MapPin, Money, Timer } from "phosphor-react";
+
 export function Success() {
   return (
-    <h1>Success</h1>
+    <div>
+      <h1>Huh! confirmed order</h1>
+      <p>Now just wait for the coffee to come to you soon</p>
+
+      <div>
+        <div>
+          <div>
+            <div>
+              <MapPin />
+            </div>
+            <div>
+              <p>Delivery at Rua João Daniel Martinelli, 102</p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <MapPin />
+            </div>
+            <div>
+              <p>Delivery at Rua João Daniel Martinelli, 102</p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <Timer />
+            </div>
+            <div>
+              <p>Delivery forecast</p>
+              <p>20 min - 30 min</p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <Money />
+            </div>
+            <div>
+              <p>Payment on delivery</p>
+              <p>Credit card</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 }
